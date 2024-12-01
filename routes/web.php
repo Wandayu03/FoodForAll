47b3c27c5c20c31c9a6b23a19009578700430774
christinesilalahi <christinesilalahi12@gmail.com>
2024-12-01 17:37:26 +0700

    Add rent page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('template');
 });
+
+Route::get('/rent/for/sharing', function () {
+    return view('rent');
+});
