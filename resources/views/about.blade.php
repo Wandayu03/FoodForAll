7e7c427acae2a8fb62071ce12cb1e33375930139
desyha <deasyahbila16@gmail.com>
2024-12-04 00:53:41 +0700

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,40 +4,40 @@
 
 @section('content')
 <div class="page">
-    <div class="about">
+    <div class="about-one">
         <img src="{{asset('assets/img/about1.jpg') }}">
-        <div>
+        <div class="about-content d-flex flex-column bd-highlight">
             <h3>About Us</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet arcu a dolor malesuada laoreet sit amet eu tortor. Fusce efficitur aliquam neque, at commodo turpis pretium sit amet. Aenean vehicula eros lacinia nibh pulvinar, nec vulputate mi ullamcorper. Nullam sed sodales tellus, quis feugiat ante. Nullam convallis turpis quis iaculis rutrum.</p>
         </div>
     </div>
-    <div>
-        <div>
+    <div class="stats d-flex flex-column flex-md-row bd-highlight justify-content-evenly">
+        <div class="stats-content d-flex flex-column bd-highlight align-items-center">
             <img src="{{asset('assets/img/about-user.png') }}">
-            <p>1.000+</p>
+            <h4>1.000+</h4>
             <p>Active users</p>
         </div>
-        <div>
+        <div class="stats-content d-flex flex-column bd-highlight align-items-center">
             <img src="{{asset('assets/img/about-donation.png') }}">
-            <p>3.000+</p>
+            <h4>3.000+</h4>
             <p>Donation gathered</p>
         </div>
-        <div>
+        <div class="stats-content d-flex flex-column bd-highlight align-items-center">
             <img src="{{asset('assets/img/about-food.png') }}">
-            <p>7.000+</p>
+            <h4>7.000+</h4>
             <p>Food shared</p>
         </div>
     </div>
-    <div>
-        <div>
+    <div class="about-two">
+        <div class="about-extended d-flex flex-column flex-md-row bd-highlight align-items-center">
             <img src="{{asset('assets/img/about2.jpg') }}">
-            <div>
+            <div class="about-extended-one d-flex flex-column bd-highlight">
                 <h3>Our Vision</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet arcu a dolor malesuada laoreet sit amet eu tortor. Fusce efficitur aliquam neque, at commodo turpis pretium sit amet. Aenean vehicula eros lacinia nibh pulvinar, nec vulputate mi ullamcorper. Nullam sed sodales tellus, quis feugiat ante. Nullam convallis turpis quis iaculis rutrum.</p>
             </div>
         </div>
-        <div>
-            <div>
+        <div class="about-extended d-flex flex-column flex-md-row bd-highlight align-items-center">
+            <div class="about-extended-two d-flex flex-column bd-highlight">
                 <h3>Our Mission</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet arcu a dolor malesuada laoreet sit amet eu tortor. Fusce efficitur aliquam neque, at commodo turpis pretium sit amet. Aenean vehicula eros lacinia nibh pulvinar, nec vulputate mi ullamcorper. Nullam sed sodales tellus, quis feugiat ante. Nullam convallis turpis quis iaculis rutrum.</p>
             </div>
