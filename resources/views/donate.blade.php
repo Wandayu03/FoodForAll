982a33ca009c401eaa2e5591f1bb413c01aed083
desyha <deasyahbila16@gmail.com>
2025-01-11 00:40:16 +0700

    secure route

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="all-wrapper">
         <!-- Form mengirim Data -->
-        <form action="{{ secure_url('donationcreate') }}" method="POST">
+        <form action="{{ secure_route('donationcreate') }}" method="POST">
             @csrf
             <!-- Pilih jumlah donasi -->
             <div class="choose-price">
